a8f2ca4bc539e5d7f38e29a85c44b736900d03b7
Rukhsar Jani <rukhsarjani440@gmail.com>
2022-12-22 09:36:20 +0500

    day6completed

--- lib/main.dart
+++ lib/main.dart
@@ -36,11 +36,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_codepur/utils/routes.dart';
 
-class Login_page extends StatelessWidget {
-  const Login_page({super.key});
+class Login_page extends StatefulWidget {
+  Login_page({super.key});
 
+
+  @override
+  State<Login_page> createState() => _Login_pageState();
+}
+
+String name = "";
+bool changebutton = false;
+
+class _Login_pageState extends State<Login_page> {
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -23,7 +32,7 @@ class Login_page extends StatelessWidget {
 
             //Text
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.bold,
@@ -40,6 +49,10 @@ class Login_page extends StatelessWidget {
                       hintText: "Enter Username",
                       labelText: 'Username',
                     ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
 
                   TextFormField(
@@ -50,20 +63,52 @@ class Login_page extends StatelessWidget {
                     ),
                   ),
 
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: ElevatedButton(
-                      child: Text(
-                        'Login',
-                        style: TextStyle(fontSize: 22),
-                      ),
-                      style: TextButton.styleFrom(minimumSize: Size(150, 50)),
-                      onPressed: () {
-                        Navigator.pushNamed(context, Myroutes.Homeroutes);
+                  SizedBox(
+                    height: 30,
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      changebutton = true;
                       
-                      },
+                      await Future.delayed(Duration(seconds: 2));
+                      Navigator.pushNamed(context, Myroutes.Homeroutes);
+                    },
+                    child: AnimatedContainer(
+                      alignment: Alignment.center,
+                      duration: Duration(seconds: 1),
+
+                      width: changebutton ? 80 : 150,
+                      height: 50,
+                      // color: Colors.deepPurple,
+                      child:changebutton? Icon(Icons.done,
+                      color: Colors.black,)
+                      :Text(
+                        "Login",
+                        style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(137, 61, 24, 24),
+                        borderRadius: BorderRadius.circular(changebutton? 15: 8),
+                      ),
                     ),
-                  )
+                  ),
+
+                  // Padding(
+                  //   padding: const EdgeInsets.only(top: 30),
+                  //   child: ElevatedButton(
+                  //     child: Text(
+                  //       'Login',
+                  //       style: TextStyle(fontSize: 22),
+                  //     ),
+                  //     style: TextButton.styleFrom(minimumSize: Size(150, 50)),
+                  //     onPressed: () {
+                  //       Navigator.pushNamed(context, Myroutes.Homeroutes);
+                  //     },
+                  //   ),
+                  // )
                 ],
               ),
             )
